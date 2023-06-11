30b73a679006d407c88d732bc49f018520be743a
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-06-11 15:23:37 +0100

    create files and js pseudocode

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// 1 Make a function for a randomly selected rock paper or scissors for the computer 
+//(thinking to use Math.random with 0-33 -34-66 67-100 each being a corresponding selection)
+// 2 Make a prompt to allow the player to select either of the three
+// 3 Create logic for rock > scissors; scissors > paper; paper > rock;
+// 4 Return if win or loss 
+// 5 increments the score
+// 6 repeat for another round (look up how to use loops)
+// 7 when at 5 points declare winner
+
