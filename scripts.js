d6b9065ca1fdb8a84525110563e00fed0074fd06
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-06-12 19:34:28 +0100

    clean up code further after more reading
    after using the additional resources in the Odin lesson I further improved readability

--- scripts.js
+++ scripts.js
@@ -1,81 +1,104 @@
-function getComputerSelection (){
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let computerSelection = undefined; 
-if (randomNumber <= 33){
+  
+  function getComputerSelection (){
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    let computerSelection = undefined; 
+    
+    if (randomNumber <= 33){
     computerSelection= "Rock";
-}   else if (randomNumber >= 67) {
+    }   
+    
+    else if (randomNumber >= 67) {
     computerSelection= "Scissors";
-}    else {
+    }    
+    
+    else {
     computerSelection= "Paper";
-}
-console.log(computerSelection)
-return computerSelection
-}
+    }
+  console.log(computerSelection)
+  return computerSelection
+  }
 
 
-function getPlayerSelection(){
-let playerInput = prompt("What move will you play?")
-let x = playerInput.toLowerCase()  
-if (x === "rock"){
+  function getPlayerSelection(){
+    let playerInput = prompt("What move will you play?")
+    let x = playerInput.toLowerCase()  
+    
+    if (x === "rock"){
     playerInput = "Rock"
-} else if (x === "paper"){
+    } 
+    
+    else if (x === "paper"){
     playerInput = "Paper"
-} else if (x === "scissors"){
+    } 
+    
+    else if (x === "scissors"){
     playerInput = "Scissors"
-} else {
+    } 
+    
+    else {
     alert ("Invalid selection, please try again.")
-}
-console.log(playerInput)
-return playerInput
-}
+    }
+  console.log(playerInput)
+  return playerInput
+  }
 //x is just a placeholder variable to make inputs case insensitive.
 // I would prefer to make it so that the player gets another attempt to input when they enter an
 // invalid option, that way a round wont be wasted in this event. 
 
-function playRound (playerSelection, computerSelection){
+  function playRound (playerSelection, computerSelection){
     if (playerSelection == computerSelection){
        return "The round is a draw";
-    } if (
+    } 
+    
+    if (
         (playerSelection == "Scissors" && computerSelection == "Rock") ||
         (playerSelection == "Rock" && computerSelection == "Paper") ||
-        (playerSelection == "Paper" && computerSelection == "Scissors")
-    )   {
-    return `Oh no! The computer has won this round! ${computerSelection} beats ${playerSelection}!`;
-    } else if (
+        (playerSelection == "Paper" && computerSelection == "Scissors") ){   
+        return `Oh no! The computer has won this round! ${computerSelection} beats ${playerSelection}!`;
+    } 
+    
+    else if (
         (playerSelection == "Rock" && computerSelection == "Scissors") ||
         (playerSelection == "Paper" && computerSelection == "Rock") ||
-        (playerSelection == "Scissors" && computerSelection == "Paper")
-    )   {
-    return `Well done! You won this round! ${playerSelection} beats ${computerSelection}!` ;
+        (playerSelection == "Scissors" && computerSelection == "Paper") ){  
+        return `Well done! You won this round! ${playerSelection} beats ${computerSelection}!` ;
     }    
 }
 
-function game(){
+  function game(){
     let playerScore = 0
     let computerScore = 0
+    
     for ( i = 0; i < 5; i++){
-    let playerSelection = getPlayerSelection();
-    let computerSelection = getComputerSelection();
+      let playerSelection = getPlayerSelection();
+      let computerSelection = getComputerSelection();
     console.log(playRound(playerSelection, computerSelection))
+    
     if (playRound(playerSelection, computerSelection) 
     == `Well done! You won this round! ${playerSelection} beats ${computerSelection}!`){;
     playerScore++
-}   else if (playRound(playerSelection, computerSelection) 
+    }  
+    
+    else if (playRound(playerSelection, computerSelection) 
 == `Oh no! The computer has won this round! ${computerSelection} beats ${playerSelection}!`){;
     computerScore++
-} else (playerScore++, computerScore++)
-} 
+    }  else (playerScore++, computerScore++)
+  } 
 // Currently the game is simply just 5 rounds all the time.
 // I would eventually like to make it become a best of 5 instead.
 
-let winner = undefined
-if (computerScore == playerScore){
+  let winner = undefined
+    if (computerScore == playerScore){
     winner = "Its a tie! Refresh the browser to play again!"
-} else if (computerScore > playerScore){
-    winner = "You lost! Refresh the browser to play again!"
-} else {
-    winner = "You win! Refresh the browser to play again!"
-} console.log(winner)
-}
+    } 
 
-game()
+    else if (computerScore > playerScore){
+    winner = "You lost! Refresh the browser to play again!"
+    }   
+    
+    else {
+    winner = "You win! Refresh the browser to play again!"
+    } console.log(winner)
+  }
+
+  game()
