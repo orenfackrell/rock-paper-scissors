b8d06fde61b54a6d9e7daf09553518089e6bbb56
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-06-16 14:17:09 +0100

    add additional text to declare winner

--- scripts.js
+++ scripts.js
@@ -1,81 +1,92 @@
-  let playerSelection;
-  let computerSelection;
-  let playerScore = 0;
-  let computerScore = 0;
-  let options = document.querySelectorAll('button');
-  const playerText = document.querySelector('.playerText');
-  const computerText = document.querySelector('.computerText');
-  const rock = document.querySelector('.rock');
-  const paper = document.querySelector('.paper');
-  const scissors = document.querySelector('.scissors');
+  let playerSelection ;
+  let computerSelection ; 
+  let playerScore = 0 ;
+  let computerScore = 0 ;
+  let options = document.querySelectorAll('button') ;
+  const playerText = document.querySelector('.playerText') ;
+  const computerText = document.querySelector('.computerText') ;
+  const scoreTextPlayer = document.querySelector('.scoreTextPlayer') ;
+  const scoreTextComputer = document.querySelector('.scoreTextComputer') ;
+  const resultText = document.querySelector('.resultText') ;
 
 
-  options.forEach(button => button.addEventListener('click', () =>{
-
-  playerSelection = button.textContent;
-  computerSelection = getComputerSelection();
-  playerText.textContent = `Player: ${playerSelection}`;
-  computerText.textContent = `Computer: ${computerSelection}`
-  console.log(playerSelection)
-  console.log(computerSelection)
-  
-  playRound(playerSelection, computerSelection)
-  console.log(  playRound(playerSelection, computerSelection))
-  }));
-
-  
   function getComputerSelection (){
     let randomNumber = Math.floor(Math.random() * 100) + 1;
+    
     let computerSelection = undefined; 
     
-    if (randomNumber <= 33){
-    computerSelection= "Rock";
+    if ( randomNumber <= 33 ) {
+    computerSelection = "Rock" ;
     }   
     
     else if (randomNumber >= 67) {
-    computerSelection= "Scissors";
+    computerSelection = "Scissors" ;
     }    
     
     else {
-    computerSelection= "Paper";
+    computerSelection = "Paper" ;
     }
   return computerSelection
   }
-
-  function playRound (playerSelection, computerSelection){
+  
+  function playRound (playerSelection, computerSelection) {
+    
     if (playerSelection == computerSelection){
        return "The round is a draw";
     } 
     
     if (
-        (playerSelection == "Scissors" && computerSelection == "Rock") ||
-        (playerSelection == "Rock" && computerSelection == "Paper") ||
-        (playerSelection == "Paper" && computerSelection == "Scissors") ){   
+        ( playerSelection == "Scissors" && computerSelection == "Rock" ) ||
+        ( playerSelection == "Rock" && computerSelection == "Paper" ) ||
+        ( playerSelection == "Paper" && computerSelection == "Scissors" ) ){   
         return `Oh no! The computer has won this round! ${computerSelection} beats ${playerSelection}!`;
     } 
     
     else if (
-        (playerSelection == "Rock" && computerSelection == "Scissors") ||
-        (playerSelection == "Paper" && computerSelection == "Rock") ||
-        (playerSelection == "Scissors" && computerSelection == "Paper") ){  
-        return `Well done! You won this round! ${playerSelection} beats ${computerSelection}!` ;
+        ( playerSelection == "Rock" && computerSelection == "Scissors" ) ||
+        ( playerSelection == "Paper" && computerSelection == "Rock" ) ||
+        ( playerSelection == "Scissors" && computerSelection == "Paper" ) ){  
+        return `Well done! You won this round! ${playerSelection} beats ${computerSelection}!`;
     }    
 }
+  options.forEach(button => button.addEventListener('click', () =>{
 
-  let winner = undefined
+  playerSelection = button.textContent;
+  computerSelection = getComputerSelection();
+  playerText.textContent = `Player: ${playerSelection}`;
+  computerText.textContent = `Computer: ${computerSelection}`;
+
+  console.log(playerSelection);
+  console.log(computerSelection);
+  
+  playRound(playerSelection, computerSelection)
+  
+  if (playRound(playerSelection, computerSelection) ===  
+    `Oh no! The computer has won this round! ${computerSelection} beats ${playerSelection}!`){
+      computerScore++
+    }
+  else if (playRound(playerSelection, computerSelection) ===  
+    `Well done! You won this round! ${playerSelection} beats ${computerSelection}!`){
+      playerScore++
+    }
+    scoreTextPlayer.textContent = `${playerScore}` ;
+    scoreTextComputer.textContent = `${computerScore}` ;
     
-  if (computerScore == playerScore){
-    winner = "Its a tie! Refresh the browser to play again!"
-    } 
-
-    else if (computerScore > playerScore){
+    
+  console.log(playRound(playerSelection, computerSelection))
+  console.log(playerScore)
+  console.log(computerScore)
+    
+  
+  let winner = null
+    
+  if (computerScore >= 5){
     winner = "You lost! Refresh the browser to play again!"
-    }   
-    
-    else {
-    winner = "You win! Refresh the browser to play again!"
     } 
-    
-    console.log(winner)
-    // Currently the game is simply just 5 rounds all the time.
-    // I would eventually like to make it become a best of 5 instead} 
+
+    else if (playerScore >= 5){
+    winner = "You win! Refresh the browser to play again!"
+    }   
+  resultText.textContent = `${winner}`
+ 
+  }));
